319d16b272220fee703eadd24420886143944639
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-07-04 17:48:29 -0300

    hooks atualizarEvento

--- src/components/Calendario/index.tsx
+++ src/components/Calendario/index.tsx
@@ -5,8 +5,8 @@ import ptBR from './localizacao/ptBR.json'
 import Kalend, { CalendarEvent, CalendarView, OnEventDragFinish } from 'kalend'
 import 'kalend/dist/styles/index.css';
 import { listaDeEventosState } from '../../state/atom';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { IEvento } from '../../interfaces/IEvento';
+import { useRecoilValue } from 'recoil';
+import useAtualizarEvento from '../../state/hooks/useAtualizarEvento';
 
 interface IKalendEvento {
   id?: number
@@ -20,7 +20,7 @@ const Calendario: React.FC = () => {
 
   const eventosKalend = new Map<string, IKalendEvento[]>();
   const eventos = useRecoilValue(listaDeEventosState);
-  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+  const AtualizarEvento = useAtualizarEvento();
 
   eventos.forEach(evento => {
     const chave = evento.inicio.toISOString().slice(0, 10)
@@ -48,10 +48,7 @@ const Calendario: React.FC = () => {
       eventoAtualizado.inicio = new Date(kalendEventoAtualizado.startAt);
       eventoAtualizado.fim = new Date(kalendEventoAtualizado.endAt);
 
-      setListaDeEventos(listaAntiga => {
-        const indice = listaAntiga.findIndex(evt => evt.id === evento.id);
-        return [...listaAntiga.slice(0, indice),eventoAtualizado, ...listaAntiga.slice(indice + 1)]
-      })
+      AtualizarEvento(eventoAtualizado)
 
     }
   };
---
+++ src/state/hooks/useAtualizarEvento.ts
@@ -0,0 +1,14 @@
+import { useSetRecoilState } from "recoil";
+import { IEvento } from "../../interfaces/IEvento";
+import { listaDeEventosState } from "../atom";
+
+
+export default function useAtualizarEvento() {
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+  return (evento: IEvento) => {
+    return setListaDeEventos(listaAntiga => {
+      const indice = listaAntiga.findIndex(evt => evt.id === evento.id);
+      return [...listaAntiga.slice(0, indice),evento, ...listaAntiga.slice(indice + 1)]
+    })
+  }
+}
